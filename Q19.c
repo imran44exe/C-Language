7a8cead4251203124c16e9c57edbcb590766725c
IMRAN <all.about.imran.44@gmail.com>
2026-05-20 20:26:00 +0530

    Print Your Name 10 times Using Loop

---
+++ Q19.c
@@ -0,0 +1,10 @@
+// W.a p to print your name 10 times using loop statement.
+#include <stdio.h>
+void main()
+{
+    int i;
+    for(i=1; i<=10; i++)
+       {
+        printf("\n My Name is IMRAN ");
+       }
+}
