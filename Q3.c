1e6e4a6d7ae6270f74777f0f8256f46db2c9fa17
IMRAN <all.about.imran.44@gmail.com>
2026-05-20 19:23:38 +0530

    Input and Sum Two Number

---
+++ Q3.c
@@ -0,0 +1,12 @@
+// Write a programme to enter two numbers from keyboard and sum them.
+#include<stdio.h>
+void main()
+{
+    int a,b,sum;
+    printf("\n Enter First Number = ");
+    scanf("%d",&a);
+    printf("\n Enter Second Number = ");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("\n Sum = %d",sum);
+}
