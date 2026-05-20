22a6fb271a6407e74678b587f3d1ff4334b8356d
IMRAN <all.about.imran.44@gmail.com>
2026-05-20 19:37:13 +0530

    Inches to Feet

---
+++ Q8.c
@@ -0,0 +1,10 @@
+// Write a programme to enter inches from keyboard and convert it into feet.
+#include<stdio.h>
+void main()
+{
+    float i,f;
+    printf("\n Enter Inches = ");
+    scanf("%f",&i);
+    f=i/12;
+    printf("\n FEET = %f ",f);
+}
