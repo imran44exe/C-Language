4b81e968119d17b8ee50692f2b82bc42b1f823cf
IMRAN <all.about.imran.44@gmail.com>
2026-05-21 13:06:27 +0530

    Input Name & Print (String)

---
+++ Q44.c
@@ -0,0 +1,10 @@
+// W.ap to enter your name from keyboard and print them.
+#include<stdio.h>
+void main()
+{
+    char name[50];
+    printf("\nEnter Your Name = ");
+    gets(name);
+    printf("\nYour Name = ");
+    puts(name);
+}
