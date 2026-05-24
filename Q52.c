b07e5860edc55aaf7903bfaa384751498a33e7d1
IMRAN <all.about.imran.44@gmail.com>
2026-05-24 11:17:35 +0530

    Print Your Name (Function)

---
+++ Q52.c
@@ -0,0 +1,10 @@
+// Wap to create a function to print your name.
+#include<stdio.h>
+void PrintMyName()
+            {
+              printf("My Name is IMRAN");  
+            }
+void main()
+        {
+         PrintMyName();   
+        }            
